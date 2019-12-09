09f95b02759a2edf9254bae920a65dc84ab75089
Harshgiri Gonsai <giri@Harshgiris-MacBook-Pro.local>
2019-12-09 19:54:48 +1300

    Combining multiple Parameters in Test Suite

--- src/test/java/com/herokuapp/theinternet/Login_Tests.java
+++ src/test/java/com/herokuapp/theinternet/Login_Tests.java
@@ -4,13 +4,68 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-public class Login_Test_Negative {
-
-	@Test(/*groups = { "smokeTest" }*/)
-	public void incorrect_username_test() {
+public class Login_Tests {
+	
+	@Parameters({ "username", "password" })
+	@Test(priority = 1, groups = { "positiveTest", "smokeTest" })
+	public void positive_LoginTest(String  username, String password) {
+		
+		//Create Browser Driver
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		//Open Main URL
+		System.out.println("Starting test method: login_Test");
+		driver.get("https://the-internet.herokuapp.com/");
+		driver.manage().window().maximize();
+		
+		//Open the Form Authentication URL
+		driver.findElement(By.xpath("//div[@id='content']/ul//a[@href='/login']")).click();
+		System.out.println("Opening the Form Authentication Page");
+		//Thread.sleep(3000);
+		
+		//Enter Username
+		System.out.println("Entering Username");
+		driver.findElement(By.id("username")).sendKeys(username);
+		
+		//Enter Password
+		System.out.println("Entering Password");
+		driver.findElement(By.id("password")).sendKeys(password);
+		
+		//Click Login
+		System.out.println("Click Login Button");
+		driver.findElement(By.xpath("//form[@id='login']//i[@class='fa fa-2x fa-sign-in']")).click();
+		
+		//Verification Steps
+		//Verify the 'secure' URL
+		var urlAssertion = driver.getCurrentUrl();
+		System.out.println("Verifying the URL");
+		Assert.assertEquals("https://the-internet.herokuapp.com/secure", urlAssertion);
+		
+		
+		//Verify the successful login button
+		System.out.println("Verifying the login div");
+		driver.findElement(By.xpath("//div[@id='flash']")).isDisplayed();
+		
+		//Verify the 'Logout' button
+		System.out.println("Verifying the logout button");
+		driver.findElement(By.xpath("//a[@class='button secondary radius']")).isDisplayed();
+		
+		//Close Browser
+		driver.quit();
+		
+		//Advanced Verification
+		//Check the HTTP Response of the Successful login page
+		//Check the API Response
+	}
+	
+	@Parameters({ "username", "password" })
+	@Test(priority = 2, groups = { "negativeTest", "smokeTest" })
+	public void negative_LoginTest_Incorrect_Username(String  username, String password) {
 
 		// Create Browser Driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
@@ -28,11 +83,11 @@ public class Login_Test_Negative {
 
 		// Enter Username
 		System.out.println("Entering Username");
-		driver.findElement(By.id("username")).sendKeys("incorrect");
+		driver.findElement(By.id("username")).sendKeys(username);
 
 		// Enter Password
 		System.out.println("Entering Password");
-		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.id("password")).sendKeys(password);
 
 		// Click Login
 		System.out.println("Click Login Button");
@@ -52,8 +107,9 @@ public class Login_Test_Negative {
 		driver.quit();
 	}
 
-	@Test(enabled = false)
-	public void incorrect_password_test() {
+	@Parameters({ "username", "password" })
+	@Test(priority = 3, groups = { "negativeTest", "smokeTest" })
+	public void negative_LoginTest_Incorrect_Password(String  username, String password) {
 
 		// Create Browser Driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
@@ -71,11 +127,11 @@ public class Login_Test_Negative {
 
 		// Enter Username
 		System.out.println("Entering Username");
-		driver.findElement(By.id("username")).sendKeys("incorrect");
+		driver.findElement(By.id("username")).sendKeys(username);
 
 		// Enter Password
 		System.out.println("Entering Password");
-		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword");
+		driver.findElement(By.id("password")).sendKeys(password);
 
 		// Click Login
 		System.out.println("Click Login Button");
@@ -94,4 +150,5 @@ public class Login_Test_Negative {
 		// Closer Browser Instance
 		driver.quit();
 	}
+
 }
