58941c2e715d97b84fa403fca4d4377f19d919b7
Harshgiri Gonsai <giri@Harshgiris-MacBook-Pro.local>
2020-01-03 02:14:04 +1300

    Addition of JS Methods for handling JS Alerts, Confirms & Prompts

--- src/main/java/com/herokuapp/theinternet/pages/JavaScriptAlertPage.java
+++ src/main/java/com/herokuapp/theinternet/pages/JavaScriptAlertPage.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 public class JavaScriptAlertPage extends BasePageObject{
 
 	private By javascriptAlertButtonLocator = By.xpath("//button[text()='Click for JS Alert']");
+	private By javascriptConfirmButtonLocator = By.xpath("//button[text()='Click for JS Confirm']");
+	private By javascriptPromptButtonLocator = By.xpath("//button[text()='Click for JS Prompt']");
+	private By javascriptResultLocator = By.id("id");
 	
 	public JavaScriptAlertPage(WebDriver driver, Logger log) {
 		super(driver, log);
@@ -20,8 +23,6 @@ public class JavaScriptAlertPage extends BasePageObject{
 	}	
 	
 	//Get the text from the Alert
-	
-	
 	//Click on OK
 	//Get the text from Result Tag (Verify using 'contains' assertion)
 	//--------------------------------------------------------------//
@@ -29,6 +30,10 @@ public class JavaScriptAlertPage extends BasePageObject{
 	
 	//--------------------------------------------------------------//
 	//Method for JS Confirm
+	public void clickJSConfirm() {
+		log.info("Clicking on button for JS Confirm");
+		click(javascriptConfirmButtonLocator);
+	}
 	//Get the text from the JS Confirm
 	//Click Cancel
 	//Get the Text from Result Tag (Verify using 'contains' assertion)
@@ -40,11 +45,22 @@ public class JavaScriptAlertPage extends BasePageObject{
 	
 	//--------------------------------------------------------------//
 	//Method for JS Prompt
+	public void clickJSPrompt() {
+		log.info("Clicking on button for JS Prompt");
+		click(javascriptPromptButtonLocator);
+	}
 	//Get the text from JS Prompt
 	//Sendkeys to the textbox to the Prompt
 	//Click OK
 	//Get the Text from Result Tag (Verify using 'contains' assertion)
 	//--------------------------------------------------------------//
+	
+	//Method for getting the Result Text
+	public String getResultText() {
+		String text = find(javascriptResultLocator).getText();
+		log.info("The result for JS Action is: " + text);
+		return text;
+	}
 	
 	
 	//--------------------------------------------------------------//
